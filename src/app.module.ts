d685a348d6b4a30318aaef695e5f0261323a9cf2
minjaekim <minjae2246@gmail.com>
2022-01-08 19:04:00 +0900

    Add: auth 폴더 추가 후 domain 폴더로 이동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { ShopsModule } from './shops/shops.module';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
+import { UsersModule } from './domain/users/users.module';
+import { ShopsModule } from './domain/shops/shops.module';
+import { CategoriesModule } from './domain/categories/categories.module';
+import { ProductsModule } from './domain/products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ShopsModule, CategoriesModule, ProductsModule],
+  imports: [
+    UsersModule,
+    ShopsModule,
+    CategoriesModule,
+    ProductsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
